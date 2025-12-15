2180af188b30248c676b6f356daee12df50cb61d
riccardotaffoni <riccardo.taffoni@veosgroup.it>
2025-12-15 14:00:42 +0100

    dd

---
+++ render_build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Installa i pacchetti
+pip install -r requirements.txt
+
+# Applica le migrazioni al database
+python manage.py migrate
+
+# Raccogli i file statici
+python manage.py collectstatic --noinput
