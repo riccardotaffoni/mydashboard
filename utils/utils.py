9339d3ea400dd7a497879e86b6a989a1c97fffcb
riccardotaffoni <riccardo.taffoni@veosgroup.it>
2025-12-24 09:48:35 +0100

    ss

--- utils/utils.py
+++ utils/utils.py
@@ -1,16 +1,16 @@
 from io import BytesIO
 import paramiko
 import pandas as pd
-
+import streamlit as st
 def read_from_ftp(
     filename,
     path="opt/veostrading/veos_ita_automation/",
 ):
     # SSH connection details
-    hostname = "poet.veos.digital"
+    hostname = st.secrets["FTP_HOST"]
     port = 4022
-    username = "riccardo.taffoni"
-    password = "VeosGroup2024!"  # lascia vuoto se usi solo la chiave
+    username = st.secrets["username"]
+    password = st.secrets["password"]  # lascia vuoto se usi solo la chiave
 
     remote_filepath = f"../../{path}{filename}"
 
